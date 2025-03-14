246f2d97047fbd44ad9a301044a640cbf9041fdb
islam_debbi <islamdebbi6@gmail.com>
2025-03-14 03:54:23 +0100

    Revert "-filtter doctors by id"
    This reverts commit 47df3c1de40a22a0cccea3afc90efba991fda7c9.

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,46 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/helpers/spacing.dart';
+import 'package:flutter_complete_project/features/home/ui/widgets/doctors_list/doctros_bloc_builder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../core/helpers/spacing.dart';
-import 'widgets/doctor_speciality_see_all_list.dart';
-import 'widgets/doctors_blue_container.dart';
-import 'widgets/home_top_bar.dart';
-import 'widgets/specializations_and_doctors_bloc_builder.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'widgets/doctors_blue_container.dart';
+import 'widgets/doctors_speciality_see_all.dart';
+import 'widgets/home_top_bar.dart';
+import 'widgets/specializations_list/specializations_bloc_builder.dart';
+
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
-        title: const HomeTopBar(),
-      ),
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          margin: const EdgeInsets.fromLTRB(20, 0, 20, 28),
+          margin: const EdgeInsets.fromLTRB(
+            20.0,
+            16.0,
+            20.0,
+            28.0,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const HomeTopBar(),
               const DoctorsBlueContainer(),
-              verticalSpace(16.h),
-              const DoctorSpecialitySeeAllList(),
-              verticalSpace(16.h),
-              const SpecializationsAndDoctorsBlocBuilder(),
-
-              //   const DoctorSpecialityListView(),
-              //   verticalSpace(16.h),
-              //   const RecommendationDoctorSeeAllList(),
-              //   verticalSpace(16.h),
-              //   const DoctorsListView(),
+              verticalSpace(24),
+              const DoctorsSpecialitySeeAll(),
+              verticalSpace(18),
+              const SpecializationsBlocBuilder(),
+              verticalSpace(8),
+              const DoctorsBlocBuilder(),
             ],
           ),
         ),
--- lib/features/home/ui/widgets/doctors_speciality_see_all.dart
+++ lib/features/home/ui/widgets/doctors_speciality_see_all.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/theming/styles.dart';
 
-import '../../../../core/theming/styles.dart';
-
-class DoctorSpecialitySeeAllList extends StatelessWidget {
-  const DoctorSpecialitySeeAllList({super.key});
+class DoctorsSpecialitySeeAll extends StatelessWidget {
+  const DoctorsSpecialitySeeAll({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Text(
-          'Doctor Speciality',
+          'Doctors Speciality',
           style: TextStyles.font18DarkBlueSemiBold,
         ),
         const Spacer(),
         Text(
-          'See all',
+          'See All',
           style: TextStyles.font12BlueRegular,
         ),
       ],
